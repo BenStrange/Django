8274845e277040c76ccfdddea4bd6651d305648c
BenStrange <ben.strange@ivendi.com>
2024-02-01 21:38:13 +0000

    405 Issue with Logout Page

--- notes/views.py
+++ notes/views.py
@@ -1,24 +1,50 @@
-from django.shortcuts import render
-from django.http import Http404
-from django.views.generic import DetailView, ListView
-
+from typing import Any
+from django.http import HttpResponseRedirect
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
 from .models import Notes
+from .forms import NotesForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class NotesListView(ListView):
+
+class NoteDeleteView(DeleteView):
     model = Notes
-    context_object_name = 'notes'
-    template_name = 'notes/notes_list.html'
-    
-    
-class NotesDetailView(DetailView):
+    success_url = "/smart/notes"
+    template_name = "notes/notes_delete.html"
+    login_url = "/admin"
+
+
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
-    context_object_name = 'note'
-    template_name = 'notes/notes_details.html'
+    success_url = "/smart/notes"
+    form_class = NotesForm
+    login_url = "/admin"
+
+class NotesCreateView(LoginRequiredMixin, CreateView):
+    model = Notes
+    success_url = "/smart/notes"
+    form_class = NotesForm
+    login_url = "/admin"
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
 
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        return render(request, 'notes/404_template.html', {}, status=404)
-    return render(request, 'notes/notes_details.html', {'note': note})
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    logging_url = "/admin"
+    login_url = "/admin"
+    def get_queryset(self):
+        return self.request.user.notes.all()
+    
+
+class NotesDetailView(LoginRequiredMixin, DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_details.html"
