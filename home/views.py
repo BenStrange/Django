911dfd567e4c427d9e1ede7ef459159cb8fd6e5a
BenStrange <ben.strange@ivendi.com>
2024-02-02 09:02:45 +0000

    Ess training complete

--- home/views.py
+++ home/views.py
@@ -3,6 +3,20 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    http_method_names = ['get', 'post']
+    success_url = '/smart/notes'
+    
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/smart/notes')
+        return super().get(request, *args, **kwargs)
+
 
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
